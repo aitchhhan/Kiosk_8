a1aa1c3851eb9797dc1b693bed159153d2fa31d3
han-mg <alsrb1277@gmail.com>
2024-06-07 13:28:04 +0900

    [add] edit, delete item

--- cafe/urls.py
+++ cafe/urls.py
@@ -8,13 +8,15 @@ urlpatterns = [
     # manager_pages
     path('manager/', manager, name='manager'),
     path('menu_manage/', menu_manage, name='menu_manage'),
+    path('menu_list/', menu_list, name='menu_list'),
     path('manager_login/', manager_login, name='manager_login'),
     path('manager_logout/', manager_logout, name='manager_logout'),
     path('add_item/', add_item, name='add_item'),
+    path('edit_item/<int:item_id>', edit_item, name='edit_item'),
     
     
     # user_pages
-    path('coffee/', coffee, name='coffee'),
+    path('menu/', menu, name='menu'),
     path('decaffein/', decaffein, name='decaffein'),
     path('dessert/', dessert, name='dessert'),
     path('tea_ade/', tea_ade, name='tea_ade'),
--- cafe/views.py
+++ cafe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from functools import wraps
 from .models import *
 from django.contrib import messages
@@ -98,14 +98,46 @@ def add_item(request):
         error_message = ""
     return render(request, 'manager_pages/add_item.html', {'error_message': error_message})
 
-# User_Pages #################################################################################################################
-# 쿠키 4.3 7.6 코드
-# npm개념
-# 시뮬라이즈
-# cpu 계산
+@manager_login_required
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
 
-def coffee(request):
-    items = Item.objects.filter(category='coffee')
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        
+        if action == 'save':
+            # "저장" 버튼이 클릭되었을 때 처리할 로직
+            item_name = request.POST['item_name']
+            item_price = request.POST['item_price']
+            item_image = request.FILES.get('item_image') if 'item_image' in request.FILES else item.item_image
+            category = request.POST['category']
+
+            # 데이터 유효성 검사 및 저장
+            if item_name and item_price and category:
+                item.item_name = item_name
+                item.item_price = item_price
+                item.item_image = item_image
+                item.category = category
+                item.save()
+                return redirect('menu_list')
+            else:
+                # 필요한 모든 데이터가 제출되지 않은 경우에 대한 처리
+                error_message = "모든 필드를 입력해야 합니다."
+                return render(request, 'manager_pages/edit_item.html', {'item': item, 'error_message': error_message})
+        
+        elif action == 'delete':
+            # "삭제" 버튼이 클릭되었을 때 처리할 로직
+            item.delete()
+            return redirect('menu_list')
+    
+    else:
+        error_message = ""
+
+    return render(request, 'manager_pages/edit_item.html', {'item': item, 'error_message': error_message})
+
+@manager_login_required
+def menu_list(request):
+    items = Item.objects.all()
     
     if request.method == 'POST':
         action = request.POST.get('action')
@@ -135,10 +167,10 @@ def coffee(request):
                 # related_stamp_collections = stamp_collection.objects.filter(stamp__event_name=ori_name)
                 # related_stamp_collections.update(stamp=new_stamp_instance)
                 
-                return redirect('coffee')  # 수정 후 도장 목록으로 리디렉션
+                return redirect('menu_list')  # 수정 후 도장 목록으로 리디렉션
 
             except Item.DoesNotExist:
-                    return render(request, 'user_pages/coffee.html', {'error_message': '필드를 확인해주세요.'})
+                    return render(request, 'manager_pages/menu_list.html', {'error_message': '필드를 확인해주세요.'})
 
         # 삭제 눌렀을 때
         if action == 'delete':
@@ -148,11 +180,68 @@ def coffee(request):
                 delstamp = Item.objects.get(event_name=ori_stamp)  # 해당 ID의 스탬프 객체를 가져옴
                 delstamp.delete()  # 스탬프 삭제
                 
-                return redirect('coffee')  # 삭제 후 리다이렉트
+                return redirect('menu_list')  # 삭제 후 리다이렉트
             except Item.DoesNotExist:
-                return render(request, 'user_pages/coffee.html', {'error_message': '이벤트가 존재하지 않습니다.'})
+                return render(request, 'manager_pages/menu_list.html', {'error_message': '이벤트가 존재하지 않습니다.'})
     
-    return render(request, 'coffee', {'items': items})
+    return render(request, 'manager_pages/menu_list.html', {'items': items})
+
+
+# User_Pages #################################################################################################################
+# 쿠키 4.3 7.6 코드
+# npm개념
+# 시뮬라이즈
+# cpu 계산
+
+def menu(request):
+    items = Item.objects.all()
+    
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        
+        # 저장 눌렀을 때
+        if action == 'save':
+            updated_data = {'item_name': request.POST.get('item_name'),
+                            'item_price': request.POST.get('item_price'),}
+            
+            ori_name = request.POST.get('ori_name')
+            new_name = request.POST.get('event_name')
+            
+            # 스탬프 모델에 데이터 저장/이미지 입력
+            images = request.FILES.get('item_image')
+            try:
+                if images:
+                    fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT))
+                    filename = fs.save(images.name, images)
+                    updated_data['image'] = filename
+                    
+                # DB에 직접 업뎃
+                Item.objects.filter(event_name=ori_name).update(**updated_data)
+                
+                new_item_instance = Item.objects.get(item_name=new_name)  # 새로운 event_name에 해당하는 stamp 인스턴스
+
+                # 2. 연결된 stamp_collection 레코드 찾기 및 외래 키 값 업데이트
+                # related_stamp_collections = stamp_collection.objects.filter(stamp__event_name=ori_name)
+                # related_stamp_collections.update(stamp=new_stamp_instance)
+                
+                return redirect('menu')  # 수정 후 도장 목록으로 리디렉션
+
+            except Item.DoesNotExist:
+                    return render(request, 'user_pages/menu.html', {'error_message': '필드를 확인해주세요.'})
+
+        # 삭제 눌렀을 때
+        if action == 'delete':
+            ori_stamp = request.POST.get('ori_name')  # 삭제할 스탬프의 ID를 받아옴
+            
+            try:
+                delstamp = Item.objects.get(event_name=ori_stamp)  # 해당 ID의 스탬프 객체를 가져옴
+                delstamp.delete()  # 스탬프 삭제
+                
+                return redirect('menu')  # 삭제 후 리다이렉트
+            except Item.DoesNotExist:
+                return render(request, 'user_pages/menu.html', {'error_message': '이벤트가 존재하지 않습니다.'})
+    
+    return render(request, 'user_pages/menu.html', {'items': items})
 
 def decaffein(request):
     items = Item.objects.filter(category='decaffein')
